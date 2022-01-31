90de5faab7069c126774c1f8e2bcd8ca047927bb
Thiago Nascimbeni <tnascimbeni@gmail.com>
2022-01-31 15:44:21 -0300

    Made profile responsive

--- src/pages/SingleUser.jsx
+++ src/pages/SingleUser.jsx
@@ -76,7 +76,7 @@ function SingleUser() {
                 </div>
             </div>
 
-            <div className="w-full rounded-lg shadow-md bg-base-100 stats ">
+            <div className="w-full rounded-lg shadow-md bg-base-100 stats grid grid-rows-2">
               {location && (
                 <div className="stat">
                   <div className="stat-title text-md">Location</div>
